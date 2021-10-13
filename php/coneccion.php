8c621d425c7299e0fb8bf75c5dd748267fbc9f64
barcoMarquez <pablob.marquez@hotmail.com>
2021-10-13 15:51:27 -0500

    change priveleges user php Conneciones

--- php/coneccion.php
+++ php/coneccion.php
@@ -5,8 +5,8 @@ function connection(){
     $conexion = null;
     $host     = 'ec2-18-188-129-60.us-east-2.compute.amazonaws.com'; #ip o nombre del servidor remoto o local
     $database = 'testConneciondb'; //SE ENCUENTRA TODAS LAS ENTIDADES REGISTRADAS
-    $username = 'admin';
-    $password = '123'; #PASSWIRD #PASSWORD SERVIDOR REMOTO ==> Pablo_1997
+    $username = 'phpConnection';
+    $password = '1997barco07pablo97'; #PASSWIRD #PASSWORD SERVIDOR REMOTO ==> Pablo_1997
     $utf8mb4  = 'utf8mb4';
 
     try{
