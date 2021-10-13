58e152914745408dfbeda7b4bdef4352f8c8acd9
barcoMarquez <pablob.marquez@hotmail.com>
2021-10-13 15:31:27 -0500

    privilegios

--- php/coneccion.php
+++ php/coneccion.php
@@ -3,16 +3,16 @@
 
 function connection(){
     $conexion = null;
-    $host     = 'localhost'; #ip o nombre del servidor remoto o local
-    $database = 'schema_dental_entity_login'; //SE ENCUENTRA TODAS LAS ENTIDADES REGISTRADAS
-    $username = 'root';
-    $password = ''; #PASSWIRD #PASSWORD SERVIDOR REMOTO ==> Pablo_1997
+    $host     = 'ec2-18-188-129-60.us-east-2.compute.amazonaws.com'; #ip o nombre del servidor remoto o local
+    $database = 'testConneciondb'; //SE ENCUENTRA TODAS LAS ENTIDADES REGISTRADAS
+    $username = 'admin';
+    $password = '123'; #PASSWIRD #PASSWORD SERVIDOR REMOTO ==> Pablo_1997
     $utf8mb4  = 'utf8mb4';
 
     try{
         $conexion = new PDO("mysql:host=$host;dbname=$database;charset=$utf8mb4",$username, $password );
     }catch (PDOException $e){
-        echo $e;
+        echo '<pre>'.$e; 
     }
 
     if($conexion){
